82226d132823a3d3c6f0f8629114b2c5e5c405a9
Thomas Lee <tms.lee9@gmail.com>
2026-01-13 15:11:59 -0500

    implemented parser methods

--- src/json.cpp
+++ src/json.cpp
@@ -159,7 +159,15 @@ void JsonValue::dump_impl(std::string& out, int indent, int current_indent) cons
 class Parser {
 public:
     explicit Parser(const std::string& input) : input_(input), pos_(0) {}
-    JsonValue parse() {}
+    JsonValue parse() {
+        skip_whitespace();
+        auto value = parse_value();
+        skip_whitespace();
+        if(pos_ != input.size()) {
+            thow ParseError("unexpected characters after JSON", pos_);
+        }
+        return value;
+    }
 
 private:
     const std::string& input_;
@@ -187,13 +195,205 @@ private:
         }
     }
 
-    JsonValue parse_value() {}
-    JsonValue parse_null() {}
-    JsonValue parse_bool() {}
-    JsonValue parse_number() {}
-    JsonValue parse_string() {}
-    JsonValue parse_array() {}
-    JsonValue parse_object() {}
+    JsonValue parse_value() {
+        skip_whitespace();
+        char c = peek();
+        if(c == 'n') return parse_null();
+        if(c=='t' || c=='f') return parse_bool();
+        if(c=='"') return parse_string();
+        if(c=='[') return parse_arrary();
+        if(c=='{') return parse_object();
+        if(c=='-' || std::isdigit(c)) return parse_number();
+        throw ParseError("unexpected character", pos_);
+    }
+
+    JsonValue parse_null() {
+        if(input_.substr(pos_, 4) == "null") {
+            pos_+= 4;
+            return JsonValue(nullptr);
+        }
+        throw ParseError("expected 'null'", pos_);
+    }
+
+    JsonValue parse_bool() {
+        if(input_.substr(pos_, 4) == "true") {
+            pos_+=4;
+            return JsonValue(true);
+        }
+        if(input_.substr(pos_, 5) == "false") {
+            pos_+=5;
+            return JsonValue(false);
+        }
+        throw ParseError("expected 'true' or 'false'", pos_);
+    }
+
+    JsonValue parse_number() {
+        /*
+        note on valid numbers in json:
+            integer:        42, -17, 0
+            decimal:        3.14, -0.5, 0.0
+            exponent:       1e10, 2E5, 1e-3, 3.14e+2
+            negative exp:   1e-10, 5E-3
+            combined:       -3.14e-10
+        invalid:
+            .5              (no leading digit)
+            -.5             (no leading digit)
+            5.              (no digit after decimal)
+            +5              (no leading plus)
+            05              (no leading zeros except 0 itself)
+            00.5            (no leading zeros)
+            0x1F            (no hex)
+            NaN             (not allowed)
+            Infinity        (not allowed)
+        */
+
+        std::size_t start = pos_;
+        if(peek() == '-') ++pos_; //leading neg sign
+
+        //this handles all pre-decimal point & exponents
+        if(peek() == '0') {
+            /*
+                if we lead with zero, it must be:
+                1. end of the value
+                2. followed by decimal
+                3. followed by exponent
+                it CANNOT be followed by more digits
+            */
+            ++pos_;
+        } else if(std::isdigit(peek())) {
+            // only check trailing digits if leading digit is NOT zero
+            while(std::isdigit(peek())) ++pos_;
+        } else {
+            // numeric value cannot lead with a non digit
+            throw ParseError("invalid number", pos);
+        }
+        
+        //handle decimal point (all decimal must be followed by string of digits)
+        if(peek() == '.') {
+            ++pos_;
+            if(!std::isdigit(peek())) {
+                throw ParseError("invalid number", pos_);
+            }
+            while(std::isdigit(peek())) ++pos_;
+        }
+
+        //handle exponents (all exponent must be followed by +/- then a string of digits)
+        if(peek() == 'e' || peek() == 'E') {
+            ++pos_;
+            if(peek() == '+' || peek() == '-') ++pos_;
+            if(!std::isdigit(peek())) {
+                throw ParseError("invalid number", pos_);
+            }
+            while(std::isdigit(peek())) ++pos_;
+        }
+
+        std::string num_str = input_.substr(start, pos_-start);
+        return JsonValue(std::stod(num_str));
+    }
+
+    JsonValue parse_string() {
+        expect('"');
+        std::string str;
+        while(peek() != '"') {
+            if(peek() == '\0'){
+                throw ParseError("unterminated string", pos_);
+            }
+            if(peek() == '\\') {
+                //deal with escape characters
+                ++pos_;
+                char escape = consume();
+                switch(escape) {
+                    case '"': str += '"'; break;
+                    case '\\': str += '\\'; break;
+                    case '/' : str += '/'; break;
+                    case 'n' : str += '\n'; break;
+                    case 'r' : str += '\r'; break;
+                    case 't' : str += '\t'; break;
+                    case 'b' : str += '\b'; break;
+                    case 'f' : str += '\f'; break;
+                    case 'u': {
+                        //unicode escape: \uXXXX
+                        //our implementation only handles basic multilingual plane. characters above 0xFFFF requires surrogate pairs, which we dont handle.
+                        std::string hex;
+                        for(int i=0; i<4; ++i) {
+                            if(!std::isxdigit(peek())) {
+                                throw ParseError("invalid unicode escape", pos_);
+                            }
+                            hex += consume();
+                        }
+                        int codepoint = std::stoi(hex, nullptr, 16);
+                        if(codepoint < 0x80) {
+                            str += static_cast<char>(codepoint);
+                        } else if (codepoint < 0x800) {
+                            str += static_cast<char>(0xC0 | (codepoint >> 6));
+                            str += static_cast<char>(0x80 | (codepoint & 0x3F));
+                        } else {
+                            str += static_cast<char>(0xE0 | (codepoint >> 12));
+                            str += static_cast<char>(0x80 | ((codepoint >> 6) & 0x3F));
+                            str += static_cast<char>(0x80 | (codepoint & 0x3F));
+                        }
+                        break;
+                    }
+                    default:
+                        throw ParseError("invalid escape sequence", pos_-1);
+                }
+            } else {
+                str += consume();
+            }
+        }
+        expect('"');
+        return JsonValue(std::move(str));
+
+    }
+
+    JsonValue parse_array() {
+        expect('[');
+        skip_whitespace();
+        JsonArray arr;
+
+        if(peek() == ']') {
+            ++pos_;
+            return Jsonvalue(std::move(arr));
+        }
+
+        while(true) {
+            arr.push_back(parse_value());
+            skip_whitespace();
+            if(peek() == ']') {
+                ++pos_;
+                return JsonValue(std::move(arr));
+            }
+            expect(',');
+            skip_whitespace();
+        }
+    }
+
+    JsonValue parse_object() {
+        expect('{');
+        skip_whitespace();
+        JsonObject obj;
+        if(peek() == '}') {
+            ++pos_;
+            return JsonValue(std::move(obj));
+        }
+        while(true) {
+            skip_whitespace();
+            if(peek() != '"') {
+                throw ParseError("expected string key", pos_);
+            }
+            auto key = parse_string().as_string();
+            skip_whitespace();
+            expect(':');
+            auto value = parse_value();
+            obj[std::move(key)] = std::move(value);
+            skip_whitespace();
+            if(peek() == '}') {
+                ++pos_;
+                return JsonValue(std::move(obj));
+            }
+            expect(',');
+        }
+    }
 };
 
 JsonValue parse(const std::string& json){
